b8225df278ea50af1dd1cd4e8121cda580c6d73e
liwenkai <2020583117@qq.com>
2023-02-25 00:05:49 +0800

    song

--- src/views/discover/song/index.vue
+++ src/views/discover/song/index.vue
@@ -1,11 +1,18 @@
 <template>
-    song
+  <div class="flex mt-10">
+    <el-card class="w-30%">
+      <el-image
+        class="rounded-full"
+        style="width: 80%; height: 80%"
+        src="http://p2.music.126.net/SA6bW1UlPP04rFB2pj-FvQ==/109951165813403264.jpg"
+      />
+    </el-card>
+    <el-card class="w-70%"
+      ><el-tag type="danger" size="large">单曲</el-tag></el-card
+    >
+  </div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style lang="less" scoped>
-
-</style>
+<style lang="less" scoped></style>
